38ee2159adda13c2fa7667715c7432859a1c3360
AlessandroVano <alessandrovanoli.16@gmail.com>
2022-01-21 16:30:07 +0100

    inserimento in pagina chiavi array

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -11,7 +11,7 @@ class MovieController extends Controller
     public function index() {
         $movies = Movie::all();
         dump($movies);
-
-        return view('movie');
+        
+        return view('movie', compact('movies'));
     }
 }
--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -1,7 +1,22 @@
 @extends('layouts.main')
 @section('content')
 <div class="container mb-5">
-    <h1 class="mb-5">Our Film</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid deleniti odit modi voluptatem temporibus hic ad nemo quae quas provident. Voluptatem veritatis incidunt deserunt architecto est eos aliquid error omnis.</p>
+   {{--  <h1 class="mb-5">Our Film</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid deleniti odit modi voluptatem temporibus hic ad nemo quae quas provident. Voluptatem veritatis incidunt deserunt architecto est eos aliquid error omnis.</p> --}}
+    <div class="row">       
+                @foreach ( $movies as $movie )
+                <div class="card col-2 mr-5">
+                    <h5>{{ $movie->title}}</h5>
+                    <h5>{{ $movie->original_title}}</h5>
+                    <h5>{{ $movie->nationality}}</h5>
+                    <h5>{{ $movie->date}}</h5>
+                    <h5>{{ $movie->vote}}</h5>
+                </div>
+                    
+                @endforeach
+
+    </div>
+
+    
 </div>
 @endsection
