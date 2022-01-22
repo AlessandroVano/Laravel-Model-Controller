428fcbbeba046ae2229992ee20cc6a2e1226fb23
AlessandroVano <alessandrovanoli.16@gmail.com>
2022-01-22 15:06:24 +0100

    fix style

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -3,10 +3,10 @@
 <div class="container mb-5">
      <h1 class="mb-5 text-center">OUR MOVIES</h1>
    
-    <div class="row">       
+    <div class="row d-flex justify-content-center">       
                 @foreach ( $movies as $index => $movie )
                 <div class="card col-2 m-1 text-center">
-                    <a href="{{ route('detail-movie', ['id' => $index]) }}">
+                    <a class="text-decoration-none secondary-color" href="{{ route('detail-movie', ['id' => $index]) }}">
                         <div class="p-2">
                             <h5>{{ $movie->title}}</h5>
                             <h5>{{ $movie->original_title}}</h5>
